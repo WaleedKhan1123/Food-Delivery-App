b82229580c6c5a1e97c9f31bc4380d383eafba5e
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-12 17:40:35 +0500

    Food Delivery App

---
+++ frontend/src/screens/Home.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+const Home = () => {
+    return (<div>
+        <Navbar/>
+        Home
+        <Footer/>
+    </div>  );
+}
+ 
+export default Home;
