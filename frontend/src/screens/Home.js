42c81dfac54ff1088cdce114def69d9f395c43fd
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-13 11:56:45 +0500

    Food Delivery App

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -3,13 +3,14 @@ import Footer from "../components/Footer";
 const Home = () => {
     return (<div  className="">
         <Navbar/> 
-        <h1 className=" text-4xl text-center border-4 inline-block  shadow-2xl " >
+        <h1 className=" grid place-content-center  border-2 " >
             Hello World!
     </h1>
      <br/>
      <div className="flex justify-center">
-    <button type="button"  className="px-4 py-2 bg-blue-500  text-cyan-50 border rounded font-semibold   bg-blue-500 hover:bg-slate-300 hover:text-black" style={{marginTop:"100px" }} >Click me</button>
+    <button type="button"  className="px-4 py-2 bg-blue-500  text-cyan-50 border rounded font-semibold   bg-blue-500 hover:bg-blue-200 active:text-balance active:bg-blue-900 active:text-teal-50 rounded-full ease-in duration-100" style={{marginTop:"100px" }} >Click me</button>
     </div>
+    <></>
     <div className="border-4">
         <Footer />
         </div>
