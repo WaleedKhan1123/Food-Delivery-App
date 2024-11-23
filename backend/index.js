2adc9e45c905df045b3aace97366166fe1672d2b
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-23 14:07:53 +0500

    Food App

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connection } from "./db.js";
 import fooduser from "./routes/user.js";
+import bodyParser from "body-parser";
 const app = express();
 
 const port = 5000;
@@ -8,7 +9,8 @@ connection();
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", fooduser);
 app.get("/", (req, res) => {
   res.send("Hello Waleed");
